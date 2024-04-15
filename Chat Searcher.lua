f4feadbd343956e41c64cd3bbb8e76985911e02b
KyRDa <david1501@inbox.ru>
2024-04-16 01:24:00 +0300

    1.0.0

--- Chat Searcher.lua
+++ Chat Searcher.lua
@@ -6,21 +6,23 @@ require 'lib.moonloader'
 local inicfg = require 'inicfg'
 
 local cfg = inicfg.load({
-    reminder = true,
-    command = "search",
-    path = os.getenv("USERPROFILE") .. "\\Documents\\GTA San Andreas User Files\\SAMP\\chatlog.txt",
-    page_border = 30
+    settings = {
+        reminder = true,
+        command = "search",
+        path = os.getenv("USERPROFILE") .. "\\Documents\\GTA San Andreas User Files\\SAMP\\chatlog.txt",
+        page_border = 30
+    }
 }, 'Chat Searcher')
-inicfg.save(cfg, 'Chat Searcher')
+inicfg.save(cfg, 'Chat Searcher.ini')
 
 
 function main()
     while not isSampAvailable() do wait(0) end
 
-    sampRegisterChatCommand(cfg.command, Search)
+    sampRegisterChatCommand(cfg.settings.command, Search)
     sampRegisterChatCommand("searchset", Settings)
 
-    if cfg.reminder then sampAddChatMessage('{62C58D}[Chat Searcher]{FFFFFF}: /searchset /'..cfg.command, -1) end
+    if cfg.settings.reminder then sampAddChatMessage('{62C58D}[Chat Searcher]{FFFFFF}: /searchset /'..cfg.settings.command, -1) end
 
     wait(-1)
 end
@@ -38,7 +40,7 @@ function Search(arg)
     arg = string.nlower(arg):gsub("([%^%$%(%)%%%.%[%]%*%+%-%?])", "%%%1") -- regex character escaping and lowering cirillic
     local search_result = {}
 
-    for line in io.lines(cfg.path) do
+    for line in io.lines(cfg.settings.path) do
         if string.nlower(line):find(arg) then
             table.insert(search_result, line)
         end
@@ -58,7 +60,7 @@ function Search(arg)
         sampShowDialog(
             558630,
             'Search result, page ¹'..page ,
-            table.concat(search_result, '\n{FFFFFF}', (page - 1) * cfg.page_border + 1, math.min(page * cfg.page_border, #search_result)),
+            table.concat(search_result, '\n{FFFFFF}', (page - 1) * cfg.settings.page_border + 1, math.min(page * cfg.settings.page_border, #search_result)),
             page <= 1 and "Close" or 'page '.. page - 1 .. ' <<',
             page >= limit_page and "Close" or ">> page ".. page + 1,
             0
@@ -86,8 +88,9 @@ function Settings()
     lua_thread.create(function()
         ::ShowDialog::
 
-        local settings = {"Reminder of commands\t", cfg.reminder and '{008000}' or '{ff0000}', tostring(cfg.reminder),
-            "\nSearch command\t{ffa500}", cfg.command, "\nPath to chatlog\t{62C58D}chatlog.txt", "\nItem limit in search result\t{ff4c5b}", cfg.page_border}
+        local settings = {"Reminder of commands\t", cfg.settings.reminder and '{008000}' or '{ff0000}', tostring(cfg.settings.reminder),
+            "\nSearch command\t{ffa500}", cfg.settings.command, "\nPath to chatlog\t{62C58D}chatlog.txt", 
+            "\nItem limit in search result\t{ff4c5b}", cfg.settings.page_border, '\n{8b0000}Reset settings'}
         
         sampShowDialog(
             558631,
@@ -103,29 +106,37 @@ function Settings()
         local _, button, list, _ = sampHasDialogRespond(558631)
         local values = {"command", "path", "page_border"}
 
-        if list == 0 and button == 1 then
-            cfg.reminder = not cfg.reminder
-            goto ShowDialog
-        elseif button == 1 then
-            sampShowDialog(
-                558632,
-                'Chat Searcher setting',
-                "{FFFFFF}Current value:\t{62C58D}"..cfg[values[list]].."\n {808080}Set new values",
-                'Close',
-                'Change',
-                1
-            )
+        if button == 1 then
+            if list == 0  then
+                cfg.settings.reminder = not cfg.settings.reminder
+            elseif list == 4 then
+                cfg.settings = {
+                    reminder = true,
+                    command = "search",
+                    path = os.getenv("USERPROFILE") .. "\\Documents\\GTA San Andreas User Files\\SAMP\\chatlog.txt",
+                    page_border = 30
+                }
+                inicfg.save(cfg, 'Chat Searcher.ini')
+            else
+                sampShowDialog(
+                    558632,
+                    'Chat Searcher setting',
+                    "{FFFFFF}Current value:\t{62C58D}"..cfg.settings[values[list]].."\n {808080}Set new values",
+                    'Close',
+                    'Change',
+                    1
+                )
 
-            while sampIsDialogActive() do wait(100) end
+                while sampIsDialogActive() do wait(100) end
 
-            local _, button, list, input = sampHasDialogRespond(558632)
-            
-            if button == 1 then
-                cfg[values[list]] = input
-                print(cfg.command)
-                inicfg.save(cfg)
+                local _, button, list, input = sampHasDialogRespond(558632)
+                
+                if button == 1 then
+                    cfg.settings[values[list]] = input
+                    print(cfg.settings.command)
+                    inicfg.save(cfg, 'Chat Searcher.ini')
+                end
             end
-
             goto ShowDialog
         end
     end)
